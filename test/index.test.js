4a60969252b14eea473deb72b7890fb42c43cba9
YouChunMing <youyou89325@163.com>
2020-01-06 12:14:16 +0800

    修正lint

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,10 @@
-import saga,{bannerSaga} from '../src/index';
-import {fork} from  'redux-saga/effects';
+import saga, { bannerSaga } from "../src/index";
+import { fork } from "redux-saga/effects";
 
 describe("application root saga", () => {
-    it('yeild fork banner', () => {
-      const gen = saga();
-      expect(gen.next().value).toEqual(fork(bannerSaga));
-      expect(gen.next().done).toEqual(true);
-    });
+  it("yeild fork banner", () => {
+    const gen = saga();
+    expect(gen.next().value).toEqual(fork(bannerSaga));
+    expect(gen.next().done).toEqual(true);
+  });
 });
